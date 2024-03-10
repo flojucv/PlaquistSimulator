f1e2ead0dff0feb8ac81dd7b6fb92b04e12a851b
flojucv <66565404+flojucv@users.noreply.github.com>
2024-03-11 00:57:52 +0100

    delete de variable inutile

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,4 @@
 export const environment = {
-    username: "website_PlaquistSimulator",
-    password: "K0bzkBNxLvgV_yAm",
     apiUrl: 'https://api.flojucvcreator.fr',
     shareLink: 'plaquistsimulator.flojucvcreator.fr',
 };
